ee2a9a5f8914f1af2687ef26a23914f4a98f40e7
yistc <32131698+yistc@users.noreply.github.com>
2022-11-07 02:52:51 +0800

    1

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return("message": "Hello world")
+    return {"message": "Hello World"}
+
+@app.get("/id", response_class=PlainTextResponse)
+async def get_id(request: Request):
+    client_host = request.client.host
+    return client_host
+
